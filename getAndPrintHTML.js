79bdf7407764b9c5fd2ef083bfd398c04e921109
Kevin Paniccia <kevin.paniccia@gmail.com>
2018-06-11 20:56:33 +0000

    added another function

---
+++ getAndPrintHTML.js
@@ -0,0 +1,23 @@
+unction getAndPrintHTML () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+  https.get(requestOptions, function (response) {
+    response.setEncoding('utf8');
+    var buffer = {};
+    
+    response.on('data', function (chunk) {
+      buffer += chunk;
+      console.log(buffer + '\n');
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+  
+  });
+
+}
