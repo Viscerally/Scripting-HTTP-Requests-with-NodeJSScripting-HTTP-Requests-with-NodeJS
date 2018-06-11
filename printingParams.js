dc2f7f363949a2591fbd9c9142f69a9d2767ca7d
Kevin Paniccia <kevin.paniccia@gmail.com>
2018-06-11 21:11:28 +0000

    touched some files

---
+++ printingParams.js
@@ -0,0 +1,24 @@
+
+ var https = require('https');                              
+ // the host can be thought of as the domain name you want to read from,
+ // and the path is the resource - ‘/’ is the root path, but if you wanted to read a
+ // particular resource (like ‘/login/index.html’), that would be defined in the path
+ function getAndPrintHTML (options) {
+  https.get(options, function (response) {
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+    var buf = '';
+    response.on('data',function(chunk){
+      buf += chunk;
+    });
+    response.on('end', function() {
+      console.log('Response stream complete');
+      console.log(buf);
+    });
+  });
+ }
+ var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+ };
+ getAndPrintHTML(requestOptions);
